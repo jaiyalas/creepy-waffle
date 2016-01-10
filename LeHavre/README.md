2521ed7030980fd0c889a6905ba945692e39770c
Yun-Yan Chi <jaiyalas@gmail.com>
2016-01-11 06:39:24 +0800

    try to get info of mouse

--- LeHavre/README.md
+++ LeHavre/README.md
@@ -2,6 +2,8 @@
 
 ...
 
+##
+
 ## Resources
 
 ### resources
--- LeHavre/src/LeHavre.hs
+++ LeHavre/src/LeHavre.hs
@@ -6,6 +6,8 @@ import qualified SDL as SDL
 import Linear.V2 (V2(..))
 import Linear.V4 (V4(..))
 --
+import Data.Int
+--
 -- import Control.Wire
 import Control.Concurrent (threadDelay)
 --
@@ -19,11 +21,22 @@ main = do
 mainLoop :: SDL.Renderer -> IO ()
 mainLoop rdr = do
    -- events <- SDL.pollEvents
-   SDL.rendererDrawColor rdr SDL.$= V4 0 0 255 255
+   -- let yoo = map (picker . SDL.eventPayload) events
+   --     yolo = foldr max (V2 0 0) yoo
+   SDL.setMouseLocationMode SDL.AbsoluteLocation
+   p <- SDL.getMouseLocation
+   -- te <- SDL.getKeyboardState
+   SDL.rendererDrawColor rdr SDL.$= V4 0 200 255 255
    SDL.clear rdr
    SDL.present rdr
-   threadDelay 20000
+   threadDelay 500000
+   putStrLn $ (show p) -- ++ (show $ te SDL.ScancodeEscape)
    mainLoop rdr
+
+picker :: SDL.EventPayload -> V2 Int32
+picker (SDL.MouseMotionEvent mmed) =
+   SDL.mouseMotionEventRelMotion mmed
+picker _ = V2 0 0
 
 
 -- .
