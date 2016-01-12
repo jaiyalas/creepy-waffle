40c803ee18197f84ac6ac6afca8fae9f943b8cd6
jaiyalas <jaiyalas@gmail.com>
2016-01-12 20:26:39 +0800

    Lesson03 done

--- sdl2-examples/src/Lesson03.hs
+++ sdl2-examples/src/Lesson03.hs
@@ -1,3 +1,4 @@
+{-  事件處理 -}
 {-# LANGUAGE OverloadedStrings #-}
 module Lesson03 where
 --
@@ -5,9 +6,34 @@ import qualified SDL
 import Linear.V4 (V4(..))
 --
 import Control.Concurrent (threadDelay)
+import Control.Monad (unless)
 --
 import qualified Config
 --
 lesson03 :: IO ()
 lesson03 = do
-   print "lesson03"
+   SDL.initialize [SDL.InitVideo]
+   window <- SDL.createWindow "Lesson03" Config.winConfig
+   SDL.showWindow window
+   gSurface <- SDL.getWindowSurface window
+   pictureS <- SDL.loadBMP "./img/Broom.bmp"
+   -- 定義 loop
+   let -- 這裡一定要換行
+      loop = do
+         -- 擷取所有 events
+         events <- SDL.pollEvents
+         -- 檢查是不是有 QuitEvent
+         let quit = any (== SDL.QuitEvent) $ map SDL.eventPayload events
+         -- 更新畫面
+         SDL.surfaceFillRect gSurface Nothing $
+          V4 minBound minBound minBound maxBound
+         SDL.surfaceBlit pictureS Nothing gSurface Nothing
+         SDL.updateWindowSurface window
+         -- sleep and do next loop-step
+         threadDelay 20000
+         unless quit loop
+   -- 執行 loop
+   loop
+   SDL.destroyWindow window
+   SDL.freeSurface pictureS
+   SDL.quit
